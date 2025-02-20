dc325f4a8f358fb090d20d76e40b79d10257c37f
Kartal Kaan Bozdogan <kartalkaanbozdogan@gmail.com>
2025-02-20 13:47:34 +0100

    Reorg

---
+++ src/cc/cc.ts
@@ -0,0 +1,13 @@
+/* 
+
+export async function* dripCCRaw(
+  db: Db,
+  nsOrCursor: String | CCCursor,
+  rule: Rule
+): AsyncGenerator<Buffer, void, void> {}
+
+export async function* dripCC(
+  db: Db,
+  nsOrCursor: String | CCCursor,
+  rule: Rule
+): AsyncGenerator<Document, void, void> {} */
