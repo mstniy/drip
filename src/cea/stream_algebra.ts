23c17fa04dfc8ee56333bc7d17f1ff5de9eeb805
Kartal Kaan Bozdogan <kartalkaanbozdogan@gmail.com>
2025-02-23 20:21:27 +0100

    Deleted streamSubtract: Not used anymore

--- src/cea/stream_algebra.ts
+++ src/cea/stream_algebra.ts
@@ -26,32 +26,6 @@ export async function* streamAdd<T1, T2>(
   }
 }
 
-export async function* streamSubtract<T1, T2>(
-  s1: AsyncGenerator<T1, void, void>,
-  s2: AsyncGenerator<T2, void, void>,
-  lt: (a: T1 | T2, b: T1 | T2) => boolean
-): AsyncGenerator<T1, void, void> {
-  let [res1, res2] = await Promise.all([s1.next(), s2.next()]);
-  while (!res1.done && !res2.done) {
-    if (lt(res1.value, res2.value)) {
-      yield res1.value;
-      res1 = await s1.next();
-    } else if (lt(res2.value, res1.value)) {
-      res2 = await s2.next();
-    } else {
-      res1 = await s1.next();
-      res2 = await s2.next();
-    }
-  }
-  if (!res1.done) {
-    yield res1.value;
-    yield* s1;
-  }
-  if (!res2.done) {
-    s2.return();
-  }
-}
-
 type AGYieldType<T extends AsyncGenerator<any, any, any>> =
   T extends AsyncGenerator<infer TT, any, any> ? TT : unknown;
 
--- tests/stream_algebra_test.ts
+++ tests/stream_algebra_test.ts
@@ -1,11 +1,7 @@
 import { strict as assert } from "assert";
 import { describe, it } from "node:test";
 import { genToArray } from "./gen_to_array";
-import {
-  streamAdd,
-  streamSquashMerge,
-  streamSubtract,
-} from "../src/cea/stream_algebra";
+import { streamAdd, streamSquashMerge } from "../src/cea/stream_algebra";
 
 async function* streamFrom<T>(x: T[], cleanup?: () => void) {
   try {
@@ -65,65 +61,6 @@ describe("streamAdd", () => {
       ),
       [0, 0, 1, 2, 3, 4, 5]
     );
-  });
-});
-
-describe("streamSubtract", () => {
-  it("can subtract two empty streams", async () => {
-    let done = [false, false];
-    assert.deepStrictEqual(
-      await genToArray(
-        streamSubtract(
-          streamFrom([], () => (done[0] = true)),
-          streamFrom([], () => (done[1] = true)),
-          (a: number, b: number) => a < b
-        )
-      ),
-      []
-    );
-    assert.deepStrictEqual(done, [true, true]);
-  });
-  it("LHS can be empty", async () => {
-    let done = [false, false];
-    assert.deepStrictEqual(
-      await genToArray(
-        streamSubtract(
-          streamFrom([], () => (done[0] = true)),
-          streamFrom([1], () => (done[1] = true)),
-          (a: number, b: number) => a < b
-        )
-      ),
-      []
-    );
-    assert.deepStrictEqual(done, [true, true]);
-  });
-  it("RHS can be empty", async () => {
-    let done = [false, false];
-    assert.deepStrictEqual(
-      await genToArray(
-        streamSubtract(
-          streamFrom([1], () => (done[0] = true)),
-          streamFrom([], () => (done[1] = true)),
-          (a: number, b: number) => a < b
-        )
-      ),
-      [1]
-    );
-    assert.deepStrictEqual(done, [true, true]);
-  });
-  it("does subtraction", async () => {
-    let done = [false, false];
-    assert.deepStrictEqual(
-      await genToArray(
-        streamSubtract(
-          streamFrom([0, 2, 3, 4, 6], () => (done[0] = true)),
-          streamFrom([0, 1, 3, 5, 6], () => (done[1] = true)),
-          (a: number, b: number) => a < b
-        )
-      ),
-      [2, 4]
-    );
-    assert.deepStrictEqual(done, [true, true]);
   });
 });
 
