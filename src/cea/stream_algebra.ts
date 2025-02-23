b326d8845d77de20fa091cb3a4274f35c11b0955
Kartal Kaan Bozdogan <kartalkaanbozdogan@gmail.com>
2025-02-23 12:51:14 +0100

    streamSquashMerge

--- src/cea/stream_algebra.ts
+++ src/cea/stream_algebra.ts
@@ -51,3 +51,49 @@ export async function* streamSubtract<T1, T2>(
     s2.return();
   }
 }
+
+type AGYieldType<T extends AsyncGenerator<any, any, any>> =
+  T extends AsyncGenerator<infer TT, any, any> ? TT : unknown;
+
+export async function* streamSquashMerge<
+  T extends readonly AsyncGenerator<any, void, void>[]
+>(
+  ss: T,
+  lt: (a: AGYieldType<T[number]>, b: AGYieldType<T[number]>) => boolean
+): AsyncGenerator<AGYieldType<T[number]>, void, void> {
+  let ress = await Promise.all(ss.map((s) => s.next()));
+  while (true) {
+    let state:
+      | { idxs: undefined }
+      | { idxs: number[]; smallest: AGYieldType<T[number]> } = {
+      idxs: undefined,
+    };
+    for (const [idx, res] of ress.entries()) {
+      if (res.done) continue;
+      if (typeof state.idxs === "undefined" || lt(res.value, state.smallest)) {
+        state = {
+          idxs: [idx],
+          smallest: res.value,
+        };
+      } else if (!lt(state.smallest, res.value)) {
+        state = {
+          idxs: [...state.idxs, idx],
+          smallest: state.smallest,
+        };
+      }
+    }
+    if (typeof state.idxs === "undefined") {
+      // All streams are done
+      break;
+    }
+    // Yield the smallest element, giving priority
+    // to the instance coming from earlier streams
+    // if there are multiple minimums
+    yield state.smallest;
+    // Advance the relevant stream(s)
+    const news = await Promise.all(state.idxs.map((idx) => ss[idx]!.next()));
+    for (const [i, idx] of state.idxs.entries()) {
+      ress[idx] = news[i]!;
+    }
+  }
+}
--- tests/stream_algebra_test.ts
+++ tests/stream_algebra_test.ts
@@ -1,7 +1,11 @@
 import { strict as assert } from "assert";
 import { describe, it } from "node:test";
 import { genToArray } from "./gen_to_array";
-import { streamAdd, streamSubtract } from "../src/cea/stream_algebra";
+import {
+  streamAdd,
+  streamSquashMerge,
+  streamSubtract,
+} from "../src/cea/stream_algebra";
 
 async function* streamFrom<T>(x: T[], cleanup?: () => void) {
   try {
@@ -120,5 +124,44 @@ describe("streamSubtract", () => {
       [2, 4]
     );
     assert.deepStrictEqual(done, [true, true]);
+  });
+});
+
+describe("streamSquashMerge", async () => {
+  it("can have no streams", async () => {
+    assert.deepStrictEqual(
+      await genToArray(streamSquashMerge([], (a, b) => a < b)),
+      []
+    );
+  });
+  it("can have one stream", async () => {
+    assert.deepStrictEqual(
+      await genToArray(streamSquashMerge([streamFrom([1])], (a, b) => a < b)),
+      [1]
+    );
+  });
+  it("does squash merge", async () => {
+    assert.deepStrictEqual(
+      await genToArray(
+        streamSquashMerge(
+          [
+            streamFrom([0, 3]),
+            streamFrom([0, 1, 3, 4]),
+            streamFrom([0, 2, 3, 5]),
+          ],
+          (a, b) => a < b
+        )
+      ),
+      [0, 1, 2, 3, 4, 5]
+    );
+  });
+  it("yields items from earlier streams in case of equality", async () => {
+    const a1 = {},
+      a2 = {};
+    const res = await genToArray(
+      streamSquashMerge([streamFrom([a1]), streamFrom([a2])], (a, b) => false)
+    );
+    assert.equal(res.length, 1);
+    assert(res[0] === a1); // and not a2
   });
 });
