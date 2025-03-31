98f41c249076d2ff646c8e207962feadb0be9b52
Kartal Kaan Bozdogan <kartalkaanbozdogan@gmail.com>
2025-04-01 01:31:12 +0200

    Cleaner

---
+++ src/cleaner/cleaner.ts
@@ -0,0 +1,65 @@
+import { Db, ObjectId, ReadConcernLevel, Timestamp } from "mongodb";
+import { derivePCSCollName } from "../cea/derive_pcs_coll_name";
+import z from "zod";
+
+export async function expirePCSEvents(
+  collectionName: string,
+  metadataDb: Db,
+  until: Date
+): Promise<void> {
+  const pcsColl = metadataDb.collection(derivePCSCollName(collectionName));
+
+  const lastExpiredCT = z
+    .object({ ct: z.instanceof(Timestamp) })
+    .optional()
+    .parse(
+      (
+        await pcsColl
+          .find(
+            { w: { $lt: until } },
+            { readConcern: ReadConcernLevel.majority }
+          )
+          .sort({ w: -1 })
+          .project({ _id: 0, ct: 1 })
+          .limit(1)
+          .toArray()
+      )[0]
+    )?.ct;
+
+  if (!lastExpiredCT) {
+    // No expired events
+    return;
+  }
+
+  // Cannot use deleteMany - we must guarantee that
+  // we never delete an event with a higher
+  // [cluster time, id] followed by a lower one.
+  const idZodSchema = z.object({ _id: z.instanceof(ObjectId) });
+  const expiredEvents = pcsColl
+    .find(
+      { ct: { $lte: lastExpiredCT } },
+      { readConcern: ReadConcernLevel.majority }
+    )
+    .sort({ ct: 1, _id: 1 })
+    .project({ _id: 1 })
+    .map((o) => idZodSchema.parse(o));
+
+  while (await expiredEvents.hasNext()) {
+    const ids = expiredEvents.readBufferedDocuments();
+    // Right away schedule the next batch
+    void expiredEvents.hasNext();
+
+    const bulkDelete = ids.map((id) => {
+      return {
+        deleteOne: {
+          filter: { _id: id._id },
+        },
+      };
+    });
+
+    await pcsColl.bulkWrite(bulkDelete, {
+      ordered: true, // important - see above
+      writeConcern: { w: "majority" },
+    });
+  }
+}
---
+++ tests/cleaner/cleaner_test.ts
@@ -0,0 +1,89 @@
+import { MongoClient, Db, ObjectId, Timestamp, Collection } from "mongodb";
+import { after, before, beforeEach, describe, it } from "node:test";
+import { openTestDB } from "../test_utils/open_test_db";
+import { getRandomString } from "../test_utils/random_string";
+import { PCSNoopEvent, PCSInsertionEvent } from "../../src/cea/pcs_event";
+import { derivePCSCollName } from "../../src/cea/derive_pcs_coll_name";
+import { expirePCSEvents } from "../../src/cleaner/cleaner";
+import { advanceDate, ONE_YEAR_MS } from "../test_utils/date_utils";
+import { strict as assert } from "assert";
+
+describe("cleaner", () => {
+  let client: MongoClient;
+  let db: Db;
+  let collectionName: string;
+  let pcsCollection: Collection;
+  const events = [
+    {
+      _id: new ObjectId(),
+      ct: new Timestamp({ t: 1740050683, i: 0 }),
+      o: "n",
+      w: new Date("2025-02-20T11:23:44.707Z"),
+    } satisfies PCSNoopEvent,
+    {
+      _id: new ObjectId(),
+      ct: new Timestamp({ t: 1740050684, i: 0 }),
+      k: {
+        _id: "a",
+      },
+      o: "i",
+      a: {
+        _id: "a",
+        a: 0,
+      },
+    } satisfies PCSInsertionEvent,
+    {
+      _id: new ObjectId(),
+      ct: new Timestamp({ t: 1740050685, i: 0 }),
+      o: "n",
+      w: new Date("2025-02-20T11:24:44.708Z"),
+    } satisfies PCSNoopEvent,
+    {
+      _id: new ObjectId(),
+      ct: new Timestamp({ t: 1740050685, i: 1 }),
+      k: {
+        _id: "b",
+      },
+      o: "i",
+      a: {
+        _id: "b",
+        a: 0,
+      },
+    } satisfies PCSInsertionEvent,
+    {
+      _id: new ObjectId(),
+      ct: new Timestamp({ t: 1740050685, i: 2 }),
+      o: "n",
+      w: new Date("2025-02-20T11:25:44.708Z"),
+    },
+  ] as const;
+  before(async () => {
+    [client, db] = await openTestDB();
+  });
+  after(() => client.close());
+
+  beforeEach(async () => {
+    collectionName = getRandomString();
+    pcsCollection = db.collection(derivePCSCollName(collectionName));
+    await pcsCollection.insertMany(events);
+  });
+
+  it("does nothing if there are no affected events", async () => {
+    await expirePCSEvents(
+      collectionName,
+      db,
+      advanceDate(events[0].w, -ONE_YEAR_MS)
+    );
+    assert.deepStrictEqual(
+      (await pcsCollection.find().sort({ ct: 1 }).toArray()).map((o) => o._id),
+      events.map((o) => o._id)
+    );
+  });
+  it("deletes the affected events", async () => {
+    await expirePCSEvents(collectionName, db, events[4].w);
+    assert.deepStrictEqual(
+      (await pcsCollection.find().sort({ ct: 1 }).toArray()).map((o) => o._id),
+      events.slice(3).map((o) => o._id)
+    );
+  });
+});
