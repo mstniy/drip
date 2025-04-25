a51a5f1fc12a7b355f08a2a284e3f0352d155e61
Kartal Kaan Bozdogan <kartalkaanbozdogan@gmail.com>
2025-04-25 20:47:53 +0200

    Stop combining adjacent $match stages when inverting pipelines   May lead to pipeline errors if the later stage(s) use pipeline operators which might fail unless if the former stage(s) are run beforehand.

--- src/cea/invert_ppl.ts
+++ src/cea/invert_ppl.ts
@@ -1,7 +1,4 @@
-import {
-  DripPipelineStageParsed,
-  Match,
-} from "./parse_ppl/drip_pipeline_stage_parsed";
+import { DripPipelineStageParsed } from "./parse_ppl/drip_pipeline_stage_parsed";
 
 function invertStage(stage: DripPipelineStageParsed): DripPipelineStageParsed {
   if (stage.type === "match") {
@@ -13,48 +10,9 @@ function invertStage(stage: DripPipelineStageParsed): DripPipelineStageParsed {
 
   return stage;
 }
-
-export function combineAdjacentMatches(
-  pipeline: Readonly<DripPipelineStageParsed[]>
-): DripPipelineStageParsed[] {
-  const res: DripPipelineStageParsed[] = [];
-
-  let i = 0;
-  while (i < pipeline.length) {
-    let nextNonMatch = i;
-    while (
-      nextNonMatch < pipeline.length &&
-      pipeline[nextNonMatch]!.type === "match"
-    ) {
-      nextNonMatch++;
-    }
-    if (nextNonMatch === i) {
-      // Push nothing
-    } else if (nextNonMatch === i + 1) {
-      // Push the only match stage
-      res.push(pipeline[i]!);
-    } else {
-      // Combine together the adjacent match stages and push
-      res.push({
-        type: "match",
-        filter: {
-          $and: pipeline.slice(i, nextNonMatch).map((m) => (m as Match).filter),
-        },
-      });
-    }
-    if (nextNonMatch < pipeline.length) {
-      res.push(pipeline[nextNonMatch]!);
-    }
-    i = nextNonMatch + 1;
-  }
-
-  return res;
-}
-
 export function invertPipeline(
-  pipeline_: Readonly<DripPipelineStageParsed[]>
+  pipeline: Readonly<DripPipelineStageParsed[]>
 ): DripPipelineStageParsed[][] {
-  const pipeline = combineAdjacentMatches(pipeline_);
   const numMatches = pipeline.filter((s) => s.type === "match").length;
 
   if (numMatches === 0) {
--- tests/cea/invert_ppl_test.ts
+++ tests/cea/invert_ppl_test.ts
@@ -1,61 +1,6 @@
 import { describe, it } from "node:test";
-import {
-  combineAdjacentMatches,
-  invertPipeline,
-} from "../../src/cea/invert_ppl";
+import { invertPipeline } from "../../src/cea/invert_ppl";
 import { strict as assert } from "assert";
-
-describe("combineAdjacentMatches", () => {
-  it("works for empty pipelines", () => {
-    assert.deepStrictEqual(combineAdjacentMatches([]), []);
-  });
-  it("works for a single match stage", () => {
-    assert.deepStrictEqual(
-      combineAdjacentMatches([{ type: "match", filter: { a: 0 } }]),
-      [{ type: "match", filter: { a: 0 } }]
-    );
-  });
-  it("works for a single non-match stage", () => {
-    assert.deepStrictEqual(
-      combineAdjacentMatches([{ type: "addFields", fields: { a: 0 } }]),
-      [{ type: "addFields", fields: { a: 0 } }]
-    );
-  });
-  it("works for two match stages", () => {
-    assert.deepStrictEqual(
-      combineAdjacentMatches([
-        { type: "match", filter: { a: 0 } },
-        { type: "match", filter: { b: 0 } },
-      ]),
-      [{ type: "match", filter: { $and: [{ a: 0 }, { b: 0 }] } }]
-    );
-  });
-  it("works for three match stages", () => {
-    assert.deepStrictEqual(
-      combineAdjacentMatches([
-        { type: "match", filter: { a: 0 } },
-        { type: "match", filter: { b: 0 } },
-        { type: "match", filter: { c: 0 } },
-      ]),
-      [{ type: "match", filter: { $and: [{ a: 0 }, { b: 0 }, { c: 0 }] } }]
-    );
-  });
-  it("preserves non-match stages", () => {
-    assert.deepStrictEqual(
-      combineAdjacentMatches([
-        { type: "addFields", fields: { a: 0 } },
-        { type: "match", filter: { a: 0 } },
-        { type: "match", filter: { b: 0 } },
-        { type: "addFields", fields: { b: 0 } },
-      ]),
-      [
-        { type: "addFields", fields: { a: 0 } },
-        { type: "match", filter: { $and: [{ a: 0 }, { b: 0 }] } },
-        { type: "addFields", fields: { b: 0 } },
-      ]
-    );
-  });
-});
 
 describe("invertPipeline", () => {
   it("can invert pipelines with no $match", () => {
@@ -86,15 +31,6 @@ describe("invertPipeline", () => {
         { type: "match", filter: { b: 0 } },
       ]),
       [[]]
-    );
-  });
-  it("combines adjacent match stages", () => {
-    assert.deepStrictEqual(
-      invertPipeline([
-        { type: "match", filter: { a: 0 } },
-        { type: "match", filter: { b: 0 } },
-      ]),
-      [[{ type: "match", filter: { $nor: [{ $and: [{ a: 0 }, { b: 0 }] }] } }]]
     );
   });
 });
