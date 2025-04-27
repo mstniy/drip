3ce732664298b9785c75e25a20542fd8f5d26ae4
Kartal Kaan Bozdogan <kartalkaanbozdogan@gmail.com>
2025-04-27 13:02:35 +0200

    Change how we check for BSONValue-s   This is how the Node driver internally does it. It also works around a Bun bun. See https://github.com/oven-sh/bun/issues/19312

--- src/cea/scope_ppl/atoms.ts
+++ src/cea/scope_ppl/atoms.ts
@@ -1,12 +1,11 @@
 // See https://www.mongodb.com/docs/manual/reference/bson-types/
 
-import { BSONValue } from "bson";
-
 export function isComposite(v: unknown): v is object {
   return (
     typeof v === "object" &&
     v !== null &&
-    !(v instanceof BSONValue) &&
+    // See https://github.com/mongodb/js-bson/blob/7f2a6d3c1a21de23555c887928f253bf75c36ce8/src/extended_json.ts#L60
+    !("_bsontype" in v && typeof v._bsontype === "string") &&
     !(v instanceof Date) &&
     !(v instanceof RegExp)
   );
